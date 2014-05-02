6c73e286c04b9b3b2872656e50b6717fc8d5f188
Tejas Lagvankar <tejas@advantej.com>
2014-05-01 19:52:35 -0700

    Check point 1

---
+++ HelloGlass_chk_pt1/HelloGlass/src/main/java/com/advantej/glass/helloglass/MainActivity.java
@@ -0,0 +1,17 @@
+package com.advantej.glass.helloglass;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by tejas on 5/1/14.
+ */
+public class MainActivity extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
